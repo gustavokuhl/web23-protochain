b69b3809687a3677b29f71ad71a7ccb5667295e0
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-11-24 07:27:43 -0300

    testing ts types

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,1 +1,12 @@
+let x: number = 2
+let y: number = 4
+let z: number
+
+function somar(n1: number, n2: number) {
+  return n1 + n2
+}
+
+z = somar(x, y)
+
+console.log(z)
 console.log("Hack The Planet! XD")
