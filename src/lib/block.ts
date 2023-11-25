1d172aa52ff5689f31bd88641452039188cbdf96
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-11-25 10:29:30 -0300

    add comments

--- src/lib/block.ts
+++ src/lib/block.ts
@@ -1,12 +1,24 @@
+/**
+ * Black class
+ */
 export default class Block {
   index: number
   hash: string
 
+  /**
+   * Creates a new Block
+   * @param index The block index in the blockchain
+   * @param hash The hash of block
+   */
   constructor(index: number, hash: string) {
     this.index = index
     this.hash = hash
   }
 
+  /**
+   * Verify if the blockchain is valid
+   * @returns Return true if the block is valid
+   */
   isValid(): boolean {
     if (this.index < 0) return false
     if (!this.hash) return false
--- src/lib/blockchain.ts
+++ src/lib/blockchain.ts
@@ -1,8 +1,14 @@
 import Block from "./block"
 
+/**
+ * Blockchain class
+ */
 export default class Blockchain {
   blocks: Block[]
 
+  /**
+   * Creates a new Blockchain
+   */
   constructor() {
     let block = new Block(0, "genesis")
     this.blocks = [block]
