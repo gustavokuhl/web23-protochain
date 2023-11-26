60dfe84bb5f5037363a13c1384c95b7c67076140
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-11-26 09:31:22 -0300

    add blockchain server test

--- src/server/blockchainServer.ts
+++ src/server/blockchainServer.ts
@@ -6,8 +6,9 @@ import Block from "../lib/block"
 const PORT: number = 3000
 
 const app = express()
-app.use(morgan("tiny"))
 app.use(express.json())
+
+if (process.argv.includes("--run")) app.use(morgan("tiny"))
 
 const blockchain = new Blockchain()
 
@@ -40,6 +41,10 @@ app.post("/blocks", (req, res, next) => {
   else res.status(400).json(validation)
 })
 
-app.listen(PORT, () => {
-  console.log(`Blockchain server is running at ${PORT}`)
-})
+if (process.argv.includes("--run")) {
+  app.listen(PORT, () => {
+    console.log(`Blockchain server is running at ${PORT}`)
+  })
+}
+
+export { app }
