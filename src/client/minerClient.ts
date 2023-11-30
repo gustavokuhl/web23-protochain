4d9b42a1273d9b7b9257763a85d90205b893e79a
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-11-30 20:30:44 -0300

    add miner client

---
+++ src/client/minerClient.ts
@@ -0,0 +1,42 @@
+import axios from "axios"
+import BlockInfo from "../lib/blockInfo"
+import Block from "../lib/block"
+
+const BLOCKCHAIN_SERVER = "http://localhost:3000/"
+
+const minerWallet = {
+  privateKey: "1313",
+  publicKey: "r4to",
+}
+let totalMined = 0
+
+async function mine() {
+  console.log("Getting next block info...")
+
+  const { data } = await axios.get(`${BLOCKCHAIN_SERVER}blocks/next`)
+  const blockInfo = data as BlockInfo
+
+  const newBlock = Block.fromBlockInfo(blockInfo)
+
+  // @todo: add block reward tx
+
+  console.log("Starting mining block #" + blockInfo.index)
+  newBlock.mine(blockInfo.difficulty, minerWallet.publicKey)
+
+  console.log("Block mined: Sending to blockchain...")
+
+  try {
+    await axios.post(`${BLOCKCHAIN_SERVER}blocks/`, newBlock)
+    console.log("Block sent and accepted!")
+    totalMined++
+    console.log("Total mined blocks: " + totalMined)
+  } catch (err: any) {
+    console.error(err.response ? err.response.data : err.message)
+  }
+
+  setTimeout(() => {
+    mine()
+  }, 1000)
+}
+
+mine()
--- src/lib/block.ts
+++ src/lib/block.ts
@@ -1,5 +1,6 @@
 import sha256 from "crypto-js/sha256"
 import Validation from "./validation"
+import BlockInfo from "./blockInfo"
 
 /**
  * Block class
@@ -77,5 +78,18 @@ export default class Block {
     if (this.hash !== this.getHash() || !this.hash.startsWith(prefix))
       return new Validation(false, "Invalid hash.")
     return new Validation()
+  }
+
+  /**
+   * Transforms block info into block
+   * @param blockInfo The block info
+   * @returns A block
+   */
+  static fromBlockInfo(blockInfo: BlockInfo): Block {
+    const block = new Block()
+    block.index = blockInfo.index
+    block.previousHash = blockInfo.previousHash
+    block.data = blockInfo.data
+    return block
   }
 }
