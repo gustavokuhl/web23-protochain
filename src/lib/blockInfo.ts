4eff517b815ce0614b5fe96c107a04384a209897
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-11-30 19:22:17 -0300

    add block info and blockchain next block fn

---
+++ src/lib/blockInfo.ts
@@ -0,0 +1,11 @@
+/**
+ * The blockchain info interface
+ */
+export default interface BlockInfo {
+  index: number
+  previousHash: string
+  difficulty: number
+  maxDifficulty: number
+  feePerTx: number
+  data: string
+}
--- src/lib/blockchain.ts
+++ src/lib/blockchain.ts
@@ -1,5 +1,6 @@
 import Block from "./block"
 import Validation from "./validation"
+import BlockInfo from "./blockInfo"
 
 /**
  * Blockchain class
@@ -8,6 +9,7 @@ export default class Blockchain {
   blocks: Block[]
   nextIndex: number = 0
   static readonly DIFFICULTY_FACTOR = 5
+  static readonly MAX_DIFFICULTY = 62
 
   /**
    * Creates a new Blockchain
@@ -69,5 +71,26 @@ export default class Blockchain {
         )
     }
     return new Validation()
+  }
+
+  getFeePerTx(): number {
+    return 1
+  }
+
+  getNextBlock(): BlockInfo {
+    const data = new Date().toString()
+    const difficulty = this.getDifficulty()
+    const previousHash = this.getLastBlock().hash
+    const index = this.blocks.length
+    const feePerTx = this.getFeePerTx()
+    const maxDifficulty = Blockchain.MAX_DIFFICULTY
+    return {
+      data,
+      difficulty,
+      previousHash,
+      index,
+      feePerTx,
+      maxDifficulty,
+    } as BlockInfo
   }
 }
